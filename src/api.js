4bf2bea5c4cc65f4080109d22d2c21b409760b46
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-06-02 17:33:35 -0500

    Updated test cases for first feature

---
+++ src/api.js
@@ -0,0 +1,12 @@
+/**
+ * This function 
+ *  takes an events array, then uses map to create a new array with only the locations
+ *  then removes all duplicates by creating another new array using the spread operator and spreading a Set
+ * The Set will remove all duplicates
+ */
+
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+};
