f80ec6bed93ee1030ff485ff72b5986167c97924
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-06-12 17:30:22 -0500

    Integrated APIs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import './nprogress.css'
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+
 
 class App extends Component {
   state = {
