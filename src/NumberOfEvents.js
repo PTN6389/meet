2c11363b3c2c894313c519500decbd265eb84033
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-06-14 16:32:51 -0500

    Added InfoAlert and ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,23 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
     state = {
-        eventCount: 32
+        eventCount: 32,
+        errorText: ''
     }
 
     handleInputChanged = (event) => {
         const eventCount = event.target.value;
-        this.setState({ eventCount: eventCount });
+        if (eventCount < 1 || eventCount > 32) {
+            this.setState({
+                eventCount: eventCount,
+                errorText: 'Select number from 1 to 32'
+            })
+        } else {
+        this.setState({ eventCount: eventCount, errorText: '' });
         this.props.updateEvents(undefined, eventCount);
+        }
     }
 
     render() {
@@ -16,7 +25,9 @@ class NumberOfEvents extends Component {
             <div>
                 <input type="number" className="numberOfEvents"
                         value={this.state.eventCount}
-                        onChange={this.handleInputChanged}></input>
+                        onChange={this.handleInputChanged}>
+                </input>
+                <ErrorAlert text={this.state.errorText} />
             </div>
         )
     }
