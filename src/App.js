c24e5bf6e4a03d023f0ffc783bb5d53a1020a2b2
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-06-15 16:13:23 -0500

    Added pie chart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import { InfoAlert } from './Alert';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import EventGenre from './EventGenre';
 
 
 class App extends Component {
@@ -67,17 +68,22 @@ class App extends Component {
       <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
       <NumberOfEvents eventCount={this.state.eventCount} updateEvents={this.updateEvents}/>
       <h4>Events in each city</h4>
+       
+       <div className="data-vis-wrapper">
+          <EventGenre events={this.state.events} />
+          
+          <ResponsiveContainer height={400}>
+            <ScatterChart
+                margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+                <CartesianGrid />
+                <XAxis type="category" dataKey="city" name="city" />
+                <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter data={this.getData()} fill="#8884d8" />
+              </ScatterChart>
+            </ResponsiveContainer>
+       </div>
       
-      <ResponsiveContainer height={400}>
-        <ScatterChart
-            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
 
 
       <EventList events={this.state.events}/>
